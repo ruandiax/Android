1b3aa9b0092860be1f1ec487041f5d7e11d8d69b
Ruan Dias <ruancdias@outlook.com>
2022-01-14 15:36:03 -0300

    Status Bar + ScreenOn? = 100%
    Testar se o ScreenOn funcionou

--- app/src/main/java/com/example/androidproject/FullscreenActivity.kt
+++ app/src/main/java/com/example/androidproject/FullscreenActivity.kt
@@ -3,7 +3,13 @@ package com.example.androidproject
 import android.content.pm.ActivityInfo
 import android.net.Uri
 import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
+import android.text.Layout
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
+import android.view.WindowManager.LayoutParams
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -18,8 +24,21 @@ class FullscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fullscreen)
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
+        // Screen Orientation
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
+        // Tela sempre ligada
+        window.setFlags(LayoutParams.FLAG_KEEP_SCREEN_ON, LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         videov = findViewById(R.id.videoView)
         val videopath = "android.resource://com.example.androidproject/raw/testemp4"
@@ -32,5 +51,6 @@ class FullscreenActivity : AppCompatActivity() {
         }
 
     }
+
 
 }
